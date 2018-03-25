3538b14af934721636007df51b66d4fbda4cc38f
yeyezaw <zawyehtut@outlook.com>
2018-03-26 03:28:25 +0800

    heroku stepup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -36,6 +37,6 @@ app.get('/about', (req,res) => {
 });
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('server is running on port : 3000');
 });
